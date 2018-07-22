37f209e93d36330b4e4ca754a113878baf9073cc
imihito <pdsoxapq@gmail.com>
2018-07-22 13:25:53 +0900

    initial

---
+++ Modules/Request-Imasparql.psm1
@@ -0,0 +1,63 @@
+﻿enum ImasparqlOutputFormat {
+    json ; xml ; csv ; text 
+}
+
+# 結果のキャッシュ
+[hashtable]$cache = @{
+    [ImasparqlOutputFormat]::json = New-Object -TypeName 'Collections.Generic.Dictionary[string,psobject[]]'
+    [ImasparqlOutputFormat]::xml  = New-Object -TypeName 'Collections.Generic.Dictionary[string,xml]'
+    [ImasparqlOutputFormat]::csv  = New-Object -TypeName 'Collections.Generic.Dictionary[string,psobject[]]'
+    [ImasparqlOutputFormat]::text = New-Object -TypeName 'Collections.Generic.Dictionary[string,string[]]'
+}
+
+function Request-Imasparql {
+    [CmdletBinding()]Param (
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Query,
+        [ImasparqlOutputFormat]$OutputFormat = [ImasparqlOutputFormat]::json,
+        [switch]$Force
+    )
+    Set-StrictMode -Version Latest
+    $ErrorActionPreference = 'Stop'
+
+    # キャッシュにあれば情報取得＆リターン
+    [psobject]$dataFromCache = $null
+    if ( $cache[$OutputFormat].TryGetValue( $Query , [ref]$dataFromCache ) ) {
+        Write-Information 'use cache'
+        Write-Information ('output = ' + $OutputFormat)
+        Write-Information ('query = ' + $Query)
+        return $dataFromCache
+    }
+    
+    
+    Write-Information 'access'
+    [Net.WebClient]$wc = 
+        New-Object -TypeName Net.WebClient -Property @{
+            BaseAddress = 'https://sparql.crssnky.xyz/spql/imas/query'
+            Encoding    = [Text.UTF8Encoding]$false
+        }
+
+    # クエリの設定
+    [string]$escapedQuery = [uri]::EscapeDataString( $Query )
+
+    $wc.QueryString.Set( 'output', $OutputFormat.ToString() ) # 出力形式
+    $wc.QueryString.Set( 'query' , $escapedQuery )
+
+    Write-Information ('output = ' + $OutputFormat)
+    Write-Information ('escaped query = ' + $escapedQuery)
+    
+    [string]$dlTxt = $wc.DownloadString( [string]::Empty )
+    
+    [psobject]$result = switch ( $OutputFormat ) {
+        json { (ConvertFrom-Json -InputObject $dlTxt).results.bindings }
+        xml  { [xml]$dlTxt }
+        csv  { ConvertFrom-Csv -InputObject $dlTxt }
+        text { $dlTxt -split "`n" }
+    }
+    
+    $cache[$OutputFormat].Add( $Query , $result )
+    return $result
+}
+
+Export-ModuleMember -Function Request-Imasparql
---
+++ Sample/Sample_Request-Imasparql.ps1
@@ -0,0 +1,40 @@
+﻿# Import module
+[string]$repositoryRoot = 
+    [IO.Path]::GetDirectoryName(
+        [IO.Path]::GetDirectoryName( $MyInvocation.MyCommand.Definition )
+    )
+[string]$moduleRelPath = [IO.Path]::Combine('Modules' , 'Request-Imasparql.psm1')
+[string]$moduleAbsPath = [IO.Path]::Combine($repositoryRoot, $moduleRelPath)
+
+#if remove
+#Remove-Module -Name Request-Imasparql
+
+Import-Module -Name $moduleAbsPath
+
+# Sample
+
+# define query
+# https://sparql.crssnky.xyz/imas/ 内の「千早のセリフテキストを取得」のクエリ
+[string]$query = @'
+PREFIX schema: <http://schema.org/>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX imas: <https://sparql.crssnky.xyz/imasrdf/URIs/imas-schema.ttl#>
+
+SELECT *
+WHERE {
+  ?s rdf:type imas:ScriptText;
+     imas:Source ?source;
+     schema:text ?text.
+  ?source schema:name ?name;
+     filter(contains(str(?name),"千早")) #コメント
+} #order by ?text
+'@
+
+# get data
+[psobject[]]$result = 
+    Request-Imasparql -Query $query -OutputFormat csv -InformationAction Continue
+
+# format and print
+$result |
+    select -Property name,text |
+    Write-Output
